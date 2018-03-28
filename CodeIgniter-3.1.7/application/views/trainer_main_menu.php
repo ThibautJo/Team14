05e213be77d746955f117545b774064a0c2a3ed2
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-03-28 21:26:38 +0200

    links in menu aanpassen en kleine foutjes oplossen

--- CodeIgniter-3.1.7/application/views/trainer_main_menu.php
+++ CodeIgniter-3.1.7/application/views/trainer_main_menu.php
@@ -18,53 +18,92 @@
         <div id="submenu" class="kleur">
             <ul class="collapse list-unstyled pl-4 submenu-links" id="profielSubMenu" data-parent="#sidenav">
                 <li class="pt-2">
-                    <a class="nav-link1" href="#">Profiel</a>
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Profiel') ?>">Profiel</a>
                 </li>
                 <li class="pb-2">
-                    <a class="nav-link1" href="#">Aanpassen</a>
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Profiel/Aanpassen') ?>">Profiel aanpassen</a>
                 </li>
             </ul>
         </div>
     </li>
     <li class="nav-item">
-        <a class="nav-link d-flex align-items-center" href="#"><i class="material-icons md-18 mr-3">event_note</i><span class="menu-text">Agenda</span></a>
+        <a class="nav-link d-flex align-items-center" href="#agendaSubMenu" data-toggle="collapse" aria-expanded="false" aria-controls="agendaSubMenu"><i class="material-icons md-18 mr-3">event_note</i><span class="menu-text">Agenda</span></a>
+        <div id="submenu" class="kleur">
+            <ul class="collapse list-unstyled pl-4 submenu-links" id="agendaSubMenu" data-parent="#sidenav">
+                <li class="pt-2">
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Agenda') ?>">Agenda</a>
+                </li>
+                <li class="pb-2">
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Agenda/Aanpassen') ?>">Agenda beheren</a>
+                </li>
+            </ul>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link d-flex align-items-center" href="<?php echo site_url('/Trainer/Supplement') ?>"><i class="fas fa-pills md-18 mr-3"></i><span class="menu-text">Supplementen</span></a>
     </li>
     <li class="nav-item">
         <a class="nav-link d-flex align-items-center" href="#wedstrijdSubMenu" data-toggle="collapse" aria-expanded="false" aria-controls="wedstrijdSubMenu"><i class="material-icons md-18 mr-3">flag</i><span class="menu-text">Wedstrijden</span></a>
         <div id="submenu" class="kleur">
             <ul class="collapse list-unstyled pl-4 submenu-links" id="wedstrijdSubMenu" data-parent="#sidenav">
                 <li class="pt-2">
-                    <a class="nav-link1" href="<?php echo site_url('/Trainer/wedstrijden/index?pagina=weergaven') ?>">Wedstrijden</a>
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Wedstrijden/index?pagina=weergaven') ?>">Wedstrijden</a>
                 </li>
                 <li>
-                    <a class="nav-link1" href="#">Wedstrijdresultaten</a>
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Wedstrijden/index?pagina=aanpassen') ?>">Wedstrijden beheren</a>
+                </li>
+                <li>
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/wedstrijdResultaten/index?pagina=weergaven') ?>">Wedstrijdresultaten</a>
+                </li>
+                <li>
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/wedstrijdResultaten/index?pagina=aanpassen') ?>">Wedstrijdresultaten beheren</a>
+                </li>
+                <li>
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Inschrijvingen') ?>">Inschrijvingen</a>
                 </li>
                 <li class="pb-2">
-                    <a class="nav-link1" href="#">Inschrijven</a>
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Inschrijvingen/aanpassen') ?>">Inschrijvingen beheren</a>
                 </li>
             </ul>
         </div>
     </li>
     <li class="nav-item">
-        <a class="nav-link d-flex align-items-center" href="<?php echo site_url('/Trainer/Team') ?>"><i class="material-icons md-18 mr-3">group</i><span class="menu-text">Team</span></a>
+        <a class="nav-link d-flex align-items-center" href="#teamSubMenu" data-toggle="collapse" aria-expanded="false" aria-controls="teamSubMenu"><i class="material-icons md-18 mr-3">group</i><span class="menu-text">Team</span></a>
+        <div id="submenu" class="kleur">
+            <ul class="collapse list-unstyled pl-4 submenu-links" id="teamSubMenu" data-parent="#sidenav">
+                <li class="pt-2">
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Team') ?>">Team</a>
+                </li>
+                <li class="pb-2">
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Team/aanpassen') ?>">Zwemmers beheren</a>
+                </li>
+            </ul>
+        </div>
     </li>
     <li class="nav-item">
-        <a class="nav-link d-flex align-items-center" href="<?php echo site_url('/Trainer/Supplement') ?>"><i class="fas fa-pills md-18 mr-3"></i><span class="menu-text">Supplementen</span></a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link d-flex justify-content-between align-items-center" href="#"><span class="d-flex align-items-center"><i class="material-icons md-18 mr-3">notifications</i><span class="menu-text">Meldingen</span></span>
+        <a class="nav-link d-flex justify-content-between align-items-center" href="#meldingenSubMenu" data-toggle="collapse" aria-expanded="false" aria-controls="meldingenSubMenu"><span class="d-flex align-items-center"><i class="material-icons md-18 mr-3">notifications</i><span class="menu-text">Meldingen</span></span>
             <span class="menu-text"><div id="melding-menu" class="img-circle d-flex align-items-center justify-content-center">2</div></span>
-
         </a>
+        <div id="submenu" class="kleur">
+            <ul class="collapse list-unstyled pl-4 submenu-links" id="meldingenSubMenu" data-parent="#sidenav">
+                <li class="pt-2">
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Meldingen') ?>">Meldingen</a>
+                </li>
+                <li class="pb-2">
+                    <a class="nav-link1" href="<?php echo site_url('/Trainer/Meldingen/beheren') ?>">Meldingen beheren</a>
+                </li>
+            </ul>
+        </div>
         <div id="melding-menu-2" class="img-circle d-flex align-items-center justify-content-center">2</div>
     </li>
-     <li class="nav-item">
+    <li class="nav-item">
         <a class="nav-link d-flex align-items-center" href="<?php echo site_url('/Welcome/meldAf') ?>"><i class="material-icons md-18 mr-3">exit_to_app</i><span class="menu-text">Afmelden</span></a>
     </li>
+    <div id="menu-footer" class="position-absolute">
+        <p class="text-center">&copy; Trainingscentrum Wezenberg</p>
+    </div>
 </ul>
 
 <!-- Voetnoot in de verticale menu -->
 
-<div id="menu-footer" class="position-absolute">
-    <p class="text-center">&copy; Trainingscentrum Wezenberg</p>
-</div>
+
