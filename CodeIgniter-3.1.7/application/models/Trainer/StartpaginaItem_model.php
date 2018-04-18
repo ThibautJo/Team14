cd5595189f9d4ede9f2a60d76ca1d2e9397d09c5
Jolien Lauwers <r0660660@student.thomasmore.be>
2018-04-18 15:15:59 +0200

    Aanmelden - toonFout in progress / Startpagina beheren in progress

---
+++ CodeIgniter-3.1.7/application/models/Trainer/StartpaginaItem_model.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @class Supplement_model
+ * @brief Model-klasse voor supplementen
+ * 
+ * Model-klasse die alle methodes bevat om te interageren met de database-table supplement
+ */
+
+
+class Supplement_model extends CI_Model {
+
+    // +----------------------------------------------------------
+    // |    Trainingscentrum Wezenberg
+    // +----------------------------------------------------------
+    // |
+    // |    Supplement model
+    // |
+    // +----------------------------------------------------------
+    // |    Team 14
+    // +----------------------------------------------------------
+
+    /**
+     * Constructor
+     */
+    function __construct() {
+        parent::__construct();
+        
+    }
+    
+    /**
+     * Retourneert het record met id=$id uit de tabel supplement
+     * @param $id De id van het record dat opgevraagd wordt
+     * @return Het opgevraagde record
+     */
+    
+    function get($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('supplement');
+        return $query->row();
+    }
+       
+    /**
+     * Retourneert alle namen alfabetisch met hun functie uit de tabel supplement en supplementfunctie
+     * @return Een array van supplementnamen met bijhorende functieId
+     */
+    
+    function getAllByNaamSupplementWithFunctie() {
+        $this->db->order_by('naam', 'asc');
+        
+        $query = $this->db->get('supplement');
+        $supplementen = $query->result();
+        
+        $this->load->model('trainer/supplementfunctie_model');
+        
+        foreach ($supplementen as $supplement) {
+            $supplement->supplementFunctie = $this->supplementfunctie_model->get($supplement->supplementFunctieId);
+        }
+        return $supplementen;
+    }
+    
+    /**
+     * Verwijdert het record met id=$id uit de tabel supplement
+     * @param $id De id van het record dat opgevraagd wordt
+     */
+    
+    function delete($id){
+        $this->db->where('id', $id);
+        $this->db->delete('supplement');
+    }
+    
+    /**
+     * Voegt een nieuw record toe aan de tabel supplement
+     * 
+     * @param $supplement Het supplementen object waar de ingevulde data in zit
+     */
+    function insert($supplement) {
+        $this->db->insert('supplement', $supplement);
+    }
+    
+    /**
+     * Wijzigt een supplement-record uit de tabel supplement
+     * 
+     * @param $supplement Het supplementen object waar de aangepaste data in zit
+     */
+    function update($supplement) {
+        $this->db->where('id', $supplement->id);
+        $this->db->update('supplement', $supplement);
+    }
+
+}
+
+?>
+
