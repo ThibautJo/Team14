43796d82b9535a0bb5c9a5d2d41f66267adcc48e
LauwersJ <r0660660@student.thomasmore.be>
2018-04-27 11:03:42 +0200

    Gebruikersondersteuning

--- CodeIgniter-3.1.7/application/views/zwemmer/help_agenda.php
+++ CodeIgniter-3.1.7/application/views/zwemmer/help_agenda.php
@@ -11,5 +11,27 @@
 // +----------------------------------------------------------
 // |    Team 14
 // +----------------------------------------------------------
-
 ?>
+
+
+<?php echo toonAfbeelding('help_agenda_afbeelding.png', 'width="100%" height="100%"'); ?>
+<br />
+
+<div><p>
+Om je verder te helpen bij het raadplegen van je persoonlijke agenda, is hieronder een voorbeeld voorzien.
+
+1: De blokjes op de agenda geven aan welke activiteiten er op die datum/tijdstip plaatsvinden. 
+Bij aanklikken van deze activiteit-blokjes zal er meer gedetailleerde informatie getoond worden.
+
+2: De blokjes in de Supplementen-balk geven aan welke medicatie of supplementen op die datum ingenomen dienen te worden.
+Bij aanklikken van deze supplement-blokjes zal er meer gedetailleerde informatie getoond worden.
+
+3: Via de pijltjes kan er tussen de verschillende weken genavigeerd worden. 
+Om gemakkelijk naar de huidige datum terug te keren kan er op de knop 'Vandaag' geklikt worden.
+</p></div>
+
+<div><p>
+ 
+Waarvoor dient de agenda-tool?
+
+</p></div>
