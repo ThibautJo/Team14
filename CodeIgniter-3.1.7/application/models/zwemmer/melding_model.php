e15dbe627b21909372025d3f3449f6c4b83870c1
Lise Van Eyck <r0661824@student.thomasmore.be>
2018-05-16 17:42:53 +0200

    doxygen

--- CodeIgniter-3.1.7/application/models/zwemmer/melding_model.php
+++ CodeIgniter-3.1.7/application/models/zwemmer/melding_model.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @class Melding_model
+ * @brief Model-klasse voor meldingen
+ * 
+ * Model-klasse die alle methodes bevat om te interageren met de database-tabel melding
+ */
+
 class Melding_model extends CI_Model {
 
     // +----------------------------------------------------------
@@ -14,11 +21,20 @@ class Melding_model extends CI_Model {
     // |    Team 14
     // +----------------------------------------------------------
 
+    /**
+     * Constructor
+     */
     function __construct() {
         parent::__construct();
 
     }
 
+    /**
+     * Retourneert alle meldingen van bepaalde persoon
+     * 
+     * @param $persoonId De id van het record dat opgevraagt wordt
+     * @return De opgevraagde data
+     */
     public function getMeldingByPersoon($persoonId) {
         // Alle meldingen van een bepaalde persoon ophalen uit de databank
         $this->db->where('persoonId', $persoonId);
@@ -33,6 +49,12 @@ class Melding_model extends CI_Model {
         return $meldingen;
     }
 
+    /**
+     * Retourneert het record met id=$id uit de tabel melding
+     * 
+     * @param $meldingId De id van het record dat opgevraagd wordt
+     * @return Het opgevraagde record
+     */
     public function getMelding($meldingId) {
         $this->db->where('id', $meldingId);
         $query = $this->db->get('melding');
